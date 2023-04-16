09116683c6a4b4cd34b828e578c03ce48e56382c
Roshan <103645591+roshan-45@users.noreply.github.com>
2023-04-16 17:19:32 +0530

    Add files via upload

---
+++ new_client.ino
@@ -0,0 +1,64 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti WiFiMulti;
+
+const char* ssid = "tic-tak-toe";
+const char* password = "123456789";
+
+//Your IP address or domain name with URL path
+const char* serverNameBoard = "http://192.168.4.1/board";
+
+String temperature;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+ 
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+      value = httpGETRequest(serverNameBoard);
+      Serial.println("Value: " + value);
+  }
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "--"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
